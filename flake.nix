39f52d73f913b530bed99eab83f60c1b7347ec4c
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-09-25 23:20:49 +1000

    nix: add clazy to devshell

--- flake.nix
+++ flake.nix
@@ -50,7 +50,7 @@
       in
         pkgs.mkShell.override {stdenv = shell.stdenv;} {
           inputsFrom = [shell shell.plugin shell.extras];
-          packages = with pkgs; [material-symbols rubik nerd-fonts.caskaydia-cove];
+          packages = with pkgs; [clazy material-symbols rubik nerd-fonts.caskaydia-cove];
           CAELESTIA_XKB_RULES_PATH = "${pkgs.xkeyboard-config}/share/xkeyboard-config-2/rules/base.lst";
         };
     });
