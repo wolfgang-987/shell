0104fe25b2d36166fb9cc0ffff34158aa775b218
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-10-21 14:06:15 +1100

    internal: fix serviceref lsp warning

--- plugin/src/Caelestia/Services/serviceref.hpp
+++ plugin/src/Caelestia/Services/serviceref.hpp
@@ -10,7 +10,7 @@ class ServiceRef : public QObject {
     Q_OBJECT
     QML_ELEMENT
 
-    Q_PROPERTY(Service* service READ service WRITE setService NOTIFY serviceChanged)
+    Q_PROPERTY(caelestia::services::Service* service READ service WRITE setService NOTIFY serviceChanged)
 
 public:
     explicit ServiceRef(Service* service = nullptr, QObject* parent = nullptr);
